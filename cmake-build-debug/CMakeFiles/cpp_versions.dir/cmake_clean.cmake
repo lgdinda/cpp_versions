ab53a8c7b2078987b12aee5c917f51e0ae025bd6
lgdinda <lgdinda@gmail.com>
2020-04-02 16:16:40 +0200

    NEW: Integrating CLion with github

---
+++ cmake-build-debug/CMakeFiles/cpp_versions.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_versions.dir/main.cpp.o"
+  "cpp_versions.exe"
+  "cpp_versions.pdb"
+  "libcpp_versions.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_versions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
