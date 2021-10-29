999e81c250808ba76c797c1d91b83935f3b49fe8
lgdinda <lgdinda@gmail.com>
2021-10-29 17:58:43 +0200

    CHG: Exit message with lambda

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <cstdlib>
 
 int main() {
     std::cout << "\n\t*   Checking C++ versions   *\n\n";
+
+    std::atexit([](){std::cout << "Good bye!";});
     return 0;
 }
