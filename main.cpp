d3dbddf279aee367947d50986ce290df9566f246
lgdinda <lgdinda@gmail.com>
2022-01-31 18:45:14 +0100

    NEW: C++11 - atexit from cstdlib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <cstdlib>
 
 int main() {
     std::cout << "\n\t*   Checking C++ versions   *\n\n";
+    std::atexit(
+            [](){std::cout << "Good bye!";}
+            );
+
     return 0;
 }
